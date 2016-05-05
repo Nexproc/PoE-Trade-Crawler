4c49c25b38abe5393066e51fe6c64e781377b0c0
Nick Titcombe <me@nicktitcombe.com>
2016-05-05 08:24:02 -0700

    requirements.txt

--- poe_trader/core/async_functions.py
+++ poe_trader/core/async_functions.py
@@ -8,11 +8,16 @@ def process_async_data(fn, list_of_arg_tuples):
              for args in list_of_arg_tuples]
     run_tasks_then_close_loop(loop, tasks)
 
-def get_loop_and_executor(executor_size):
+def get_or_set_new_event_loop():
     loop = asyncio.get_event_loop()
     if loop.is_closed():
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
+
+    return loop
+
+def get_loop_and_executor(executor_size):
+    loop = get_or_set_new_event_loop()
     executor = ProcessPoolExecutor(executor_size)
     return loop, executor
 
