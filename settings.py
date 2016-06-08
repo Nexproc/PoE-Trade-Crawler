68143e624feec964d76b11ebe063edf14796365c
Nexproc <ntitcombe@gmail.com>
2016-06-08 00:01:46 -0700

    okay

--- settings.py
+++ settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, '/portal/static/')
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(PROJECT_ROOT, '/static/')
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
