ad8d20b8c5a01ee0ca9df61caf6eb3ed408e914c
Nexproc <ntitcombe@gmail.com>
2016-06-08 00:06:45 -0700

    ends with a slash

--- settings.py
+++ settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'portal/static')
-STATIC_URL = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'portal/static/')
+STATIC_URL = os.path.join(PROJECT_ROOT, 'static/')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'static/'),
 )
