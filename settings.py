2ba0aa873f26abb2c30b0dcc80fee2fcb89bc5a6
Nexproc <ntitcombe@gmail.com>
2016-06-07 23:55:52 -0700

    static

--- settings.py
+++ settings.py
@@ -62,6 +62,11 @@ STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 )
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
@@ -117,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
