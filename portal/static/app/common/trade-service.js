3e29da970ad405d75835bb93444d65a858248888
Nexproc <ntitcombe@gmail.com>
2016-06-10 01:40:38 -0700

    added hourly cache

--- portal/static/app/common/trade-service.js
+++ portal/static/app/common/trade-service.js
@@ -1,11 +1,20 @@
 /**
  * Created by Nick on 6/4/16.
  */
-poeTrade.factory('Trade', ['Request', function (Request) {
+poeTrade.factory('Trade', ['Request', '$q', function (Request, $q) {
     const trade = {};
 
+    var tradesForHourCache = {};
     trade.getTradesForHour = function (buyCurrencyId, sellCurrencyId, date) {
-        return Request.getTradesForHour(buyCurrencyId, sellCurrencyId, date);
+        var cacheKey = '' + buyCurrencyId + sellCurrencyId + date.getHours();
+        if (tradesForHourCache[cacheKey]) {
+            return $q.resolve(tradesForHourCache[cacheKey]);
+        }
+        return Request.getTradesForHour(buyCurrencyId, sellCurrencyId, date)
+            .then(function (response) {
+                tradesForHourCache[cacheKey] = response;
+                return response;
+            });
     };
 
     trade.getTradesForDay = function (buyCurrencyId, sellCurrencyId, date) {
